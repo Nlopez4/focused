380a27e3b729491a1309224c01641e03e4fe4b75
nataliel2514 <natalielopez2514@gmail.com>
2022-06-05 18:23:54 -0500

    add play button jsx

--- src/Entry.js
+++ src/Entry.js
@@ -1,11 +1,13 @@
 import './Entry.scss';
 import Home from './components/Home/index';
+import Play from './components/Play/index';
 
 // Entry component, "home page"
 function Entry() {
   return (
     <div className="entry">
       <Home />
+      <Play />
     </div>
   );
 }
